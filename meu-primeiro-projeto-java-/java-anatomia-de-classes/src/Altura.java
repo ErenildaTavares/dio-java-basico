c33b5d75344d22c57a0993725f0b7da32968a840
erenilda <erenyldajsilva@gmail.com>
2023-01-30 18:36:07 -0300

    primeiros codigos em java

---
+++ meu-primeiro-projeto-java-/java-anatomia-de-classes/src/Altura.java
@@ -0,0 +1,10 @@
+public class Altura {
+    public static void main(String[] args) {
+        
+        int altura = 199;
+        if (altura<=165)
+            System.out.println("BAIXO");
+            else if (altura>=165)
+            System.out.println("ALTO");
+    }
+}
