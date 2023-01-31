dfea4254e8116c05493ec8cf48fc3856128a4c68
erenilda <erenyldajsilva@gmail.com>
2023-01-31 12:00:30 -0300

    inserindo a leitura de idade

--- meu-primeiro-projeto-java-/java-anatomia-de-classes/src/Altura.java
+++ meu-primeiro-projeto-java-/java-anatomia-de-classes/src/Altura.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Altura {
     public static void main(String[] args) {
-        
-        int altura = 199;
-        if (altura<=165)
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite Sua Altura: ");
+        int altura = scan.nextInt();
+        if (altura <= 165)
             System.out.println("BAIXO");
-            else if (altura>=165)
+        else if (altura >= 165)
             System.out.println("ALTO");
+
+        scan.close();
+
     }
 }
--- meu-primeiro-projeto-java-/java-anatomia-de-classes/src/MinhaClasse.java
+++ meu-primeiro-projeto-java-/java-anatomia-de-classes/src/MinhaClasse.java
@@ -1,18 +1,17 @@
 public class MinhaClasse {
 
-public static void main (String [] args) {
-   String primeiroNome = "ery";
-   String segundoNome = "tavares";
+  public static void main(String[] args) {
+    String primeiroNome = "ery";
+    String segundoNome = "tavares";
 
-   String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-   
-   System.out.println(nomeCompleto);
-}
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
-public static String nomeCompleto (String primeiroNome, String segundoNome) {
-  return "resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
+    System.out.println(nomeCompleto);
+  }
 
-}
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return "resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
 
+  }
 
 }
